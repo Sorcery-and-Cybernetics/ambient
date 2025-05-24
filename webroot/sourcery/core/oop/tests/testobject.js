a0590cf74bb7ab6a7569ea781eae966ac126b5af
Marinus <ambient@sactide.com>
2025-05-24 05:57:24 +0200

    Test: Inheritance of composite models

--- webroot/sourcery/core/oop/tests/testobject.js
+++ webroot/sourcery/core/oop/tests/testobject.js
@@ -2,6 +2,7 @@ _.ambient.module("testobject", function(_) {
     _.define.object("testtrait", function(supermodel) {
         this._initial = undefined
         this._value = undefined
+        this._temp = undefined
 
         this.construct = function(initial) {
             if (initial) { this._initial = initial }
@@ -15,6 +16,12 @@ _.ambient.module("testobject", function(_) {
         this.get = function () {
             return this._value || this._initial
         }
+
+        this.temp = function(value) {
+            if (value === undefined) { return this._temp }
+            this._temp = value
+            return this
+        }
     })
 
     _.define.object("testobject", function(supermodel) {
@@ -24,14 +31,18 @@ _.ambient.module("testobject", function(_) {
             return("Test is working")
         })
 
-        this.testtrait = _.model.testtrait("Initial Value")
+        this.testtrait = _.model.testtrait("Initial Value").temp("Temporary Value")
+    })
 
-
+    //Inherit and override testtrait
+    _.define.testobject("testobject2", function(supermodel) {
+        this.testtrait = _.model.testtrait().temp("Temporary Value 2")
     })
 })
 
 .ontest("testobject", function(_) {    
     var test = _.model.testobject()
+    var test2 = _.model.testobject2()
 
     test.name("Test Object")
     this.test(test.name(), "Test Object")
@@ -41,5 +52,9 @@ _.ambient.module("testobject", function(_) {
 
     test.testtrait().let("New Value")
     this.test(test.testtrait().get(), "New Value")
+
+    //testing 2nd testobject
+    this.test(test2.testtrait().get(), "Initial Value")
+    this.test(test2.testtrait().temp(), "Temporary Value 2")
 })  
 
