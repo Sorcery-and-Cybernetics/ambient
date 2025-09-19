18d5ad1e00d0cef2a1f829eabc497751d57337fa
Sorcery and Cybernetics <ambient@sactide.com>
2025-09-19 04:49:10 +0200

    Update: Test update

--- webroot/sourcery/core/algo/circularlist/circularlist.js
+++ webroot/sourcery/core/algo/circularlist/circularlist.js
@@ -12,7 +12,50 @@ _.ambient.module("circularlist", function(_) {
             this.count = function() { return this._count }
             this.first = function() { return this._firstnode }
             this.last = function() { return this._firstnode? this._firstnode._prevnode: null }
+
+            this.add = function(node, index) {
+                if (!node) { throw "Error: circularlist.add. Node is null" }
+                if (!(node instanceof _.model.circularlistnode)) { throw "Error: circularlist.add. Node is not a circularlistnode" }
+
+                if (index === undefined) { index = -1 }  // default: add at tail
+
+                if (!this._firstnode) {
+                    // Insert as first
+                    this._firstnode = node
+
+                    node.unlink()
+
+                    node._list = this
+                    this._count = 1
+
+                } else if (index < 0) {
+                    // insert at tail
+                    var tail = this._firstnode._prevnode || this._firstnode
+                    node.assignafter(tail)
+
+                } else {
+                    // insert at head
+                    node.assignbefore(this._firstnode)
+                    this._firstnode = node
+                }
+
+                return node
+            }            
         })
+
+        this.foreach = function(callback) {
+            if (!this._firstnode || !callback) { return this }
+
+            var cursor = this._firstnode
+            
+            while (cursor) {
+                var nextnode = cursor.nextnode()
+                if (callback(cursor) === _.done) { break }
+                cursor = nextnode
+            }
+            
+            return this
+        }        
 
         this.debugbehavior = _.behavior(function() {
             this.debugout = function() {
--- webroot/sourcery/core/algo/circularlist/test.circularlist.js
+++ webroot/sourcery/core/algo/circularlist/test.circularlist.js
@@ -1,36 +1,65 @@
 _.ambient.module("test.circularlist")
     .ontest("circularlist", function(_) {
 
+        // helper to loop through nodes and collect values
+        function fornodes(startnode) {
+            var result = []
+            if (!startnode) { return result }
+            var cursor = startnode
+            do {
+                result.push(cursor.value())
+                cursor = cursor.nextnode()
+            } while (cursor && cursor != startnode)
+            return result
+        }
+
         //************************************************************
         // Rooted circular list test
         //************************************************************
         var list = _.model.circularlist()
 
         // create nodes and insert relative to root
-        var node3 = _.model.circularlistnode("3").assignafter(list)
-        var node1 = _.model.circularlistnode("1").assignbefore(node3)
-        var node5 = _.model.circularlistnode("5").assignafter(node3._list._prevnode) // tail insert
-        var node2 = _.model.circularlistnode("2").assignbefore(node3)
-        var node4 = _.model.circularlistnode("4").assignafter(node3)
+        var node3 = _.model.circularlistnode("3")
+        list.add(node3, -1)
 
-        this.assert(list.debugout(), ["1","2","3","4","5"], "circularlist (rooted): adding items")
+        var node1 = _.model.circularlistnode("1")
+        list.add(node1, 1)
 
+        var node5 = _.model.circularlistnode("5")
+        list.add(node5, -1)
+
+        var node2 = _.model.circularlistnode("2")
+        node2.assignbefore(node3)
+
+        var node4 = _.model.circularlistnode("4")
+        node4.assignafter(node3)
+
+        this.assert(fornodes(list.first()), ["1","2","3","4","5"], "circularlist (rooted): adding items")
+
+        // remove some nodes
         node3.destroy()
         node1.destroy()
         node5.destroy()
 
-        this.assert(list.debugout(), ["2","4"], "circularlist (rooted): removing items")
+        this.assert(fornodes(list.first()), ["2","4"], "circularlist (rooted): removing items")
 
-        var node1 = _.model.circularlistnode("1").assignbefore(node2)
-        var node5 = _.model.circularlistnode("5").assignafter(node4)
-        var node3 = _.model.circularlistnode("3").assignbefore(node4)
+        // rebuild list
+        var node1 = _.model.circularlistnode("1")
+        list.add(node1, 1)
 
-        this.assert(list.debugout(), ["1","2","3","4","5"], "circularlist (rooted): rebuilding items")
+        var node5 = _.model.circularlistnode("5")
+        list.add(node5, -1)
 
+        var node3 = _.model.circularlistnode("3")
+        node3.assignbefore(node4)
+
+        this.assert(fornodes(list.first()), ["1","2","3","4","5"], "circularlist (rooted): rebuilding items")
+
+        // add more nodes at head and tail
         _.model.circularlistnode("0").assignbefore(node1)
         _.model.circularlistnode("6").assignafter(node5)
 
-        this.assert(list.debugout(), ["0","1","2","3","4","5","6"], "circularlist (rooted): adding more items")
+        this.assert(fornodes(list.first()), ["0","1","2","3","4","5","6"], "circularlist (rooted): adding more items")
 
 
         //************************************************************
@@ -41,27 +70,11 @@ _.ambient.module("test.circularlist")
         var c = _.model.circularlistnode("c").assignafter(b)
         var d = _.model.circularlistnode("d").assignbefore(c)
 
-        // Collect values by looping manually (since no root.debugout)
-        var values = []
-        var cursor = a
-        do {
-            values.push(cursor.value())
-            cursor = cursor.nextnode()
-        } while (cursor && cursor != a)
-
-        this.assert(values, ["a","b","d","c"], "circularlist (rootless): adding items")
+        this.assert(fornodes(a), ["a","b","d","c"], "circularlist (rootless): adding items")
 
         // destroy b and c
         b.destroy()
         c.destroy()
 
-        // verify remaining order
-        var values2 = []
-        cursor = a
-        do {
-            values2.push(cursor.value())
-            cursor = cursor.nextnode()
-        } while (cursor && cursor != a)
-
-        this.assert(values2, ["a","d"], "circularlist (rootless): removing items")
+        this.assert(fornodes(a), ["a","d"], "circularlist (rootless): removing items")
     })
